eac2d20bbb7aa24ee8ec2fcb28d560c0d2ccfe84
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-03-26 14:44:46 +0900

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -13,7 +13,7 @@ from selfdrive.controls.lib.longcontrol import LongCtrlState
 from selfdrive.controls.lib.longitudinal_mpc_lib.long_mpc import LongitudinalMpc, MIN_ACCEL, MAX_ACCEL
 from selfdrive.controls.lib.longitudinal_mpc_lib.long_mpc import T_IDXS as T_IDXS_MPC
 from selfdrive.controls.lib.drive_helpers import V_CRUISE_MAX, CONTROL_N, get_speed_error
-# [DEC] DynamicExperimentalController 추가
+from selfdrive.controls.lib.vision_turn_controller import VisionTurnController
 from selfdrive.controls.lib.dynamic_experimental_controller import DynamicExperimentalController
 from selfdrive.swaglog import cloudlog
 
@@ -67,6 +67,9 @@ class LongitudinalPlanner:
     self.a_desired_trajectory = np.zeros(CONTROL_N)
     self.j_desired_trajectory = np.zeros(CONTROL_N)
     self.solverExecutionTime = 0.0
+
+    self.cruise_source = 'cruise'
+    self.vision_turn_controller = VisionTurnController(CP)
 
     self.use_cluster_speed = Params().get_bool('UseClusterSpeed')
 
@@ -143,6 +146,11 @@ class LongitudinalPlanner:
 
     # Prevent divergence, smooth in current v_ego
     self.v_desired_filter.x = max(0.0, self.v_desired_filter.update(v_ego))
+
+    # Get acceleration and active solutions for custom long mpc.
+    self.cruise_source, a_min_sol, v_cruise_sol = self.cruise_solutions(not reset_state, self.v_desired_filter.x,
+                                                                        self.a_desired, v_cruise, sm)
+    
     # Compute model v_ego error
     self.v_model_error = get_speed_error(sm['modelV2'], v_ego)
 
@@ -150,15 +158,16 @@ class LongitudinalPlanner:
       # if required so, force a smooth deceleration
       accel_limits_turns[1] = min(accel_limits_turns[1], AWARENESS_DECEL)
       accel_limits_turns[0] = min(accel_limits_turns[0], accel_limits_turns[1])
+      
     # clip limits, cannot init MPC outside of bounds
-    accel_limits_turns[0] = min(accel_limits_turns[0], self.a_desired + 0.05)
+    accel_limits_turns[0] = min(accel_limits_turns[0], self.a_desired + 0.05, a_min_sol)
     accel_limits_turns[1] = max(accel_limits_turns[1], self.a_desired - 0.05)
 
     self.mpc.set_weights(v_ego, self.a_desired, prev_accel_constraint)
     self.mpc.set_accel_limits(accel_limits_turns[0], accel_limits_turns[1])
     self.mpc.set_cur_state(self.v_desired_filter.x, self.a_desired)
     x, v, a, j = self.parse_model(sm['modelV2'])
-    self.mpc.update(sm['carState'], sm['radarState'], v_cruise, x, v, a, j)
+    self.mpc.update(sm['carState'], sm['radarState'], v_cruise_sol, x, v, a, j)
 
     self.v_desired_trajectory = np.interp(T_IDXS[:CONTROL_N], T_IDXS_MPC, self.mpc.v_solution)
     self.a_desired_trajectory = np.interp(T_IDXS[:CONTROL_N], T_IDXS_MPC, self.mpc.a_solution)
@@ -188,13 +197,32 @@ class LongitudinalPlanner:
     longitudinalPlan.jerks = self.j_desired_trajectory.tolist()
 
     longitudinalPlan.hasLead = sm['radarState'].leadOne.status
-    longitudinalPlan.longitudinalPlanSource = self.mpc.source
+    longitudinalPlan.longitudinalPlanSource = self.mpc.source if self.mpc.source != 'cruise' else self.cruise_source
     longitudinalPlan.fcw = self.fcw
 
     longitudinalPlan.solverExecutionTime = self.mpc.solve_time
+
+    longitudinalPlan.visionTurnControllerState = self.vision_turn_controller.state
+    longitudinalPlan.visionTurnSpeed = float(self.vision_turn_controller.v_turn)
     longitudinalPlan.e2eBlended = self.mpc.mode
 
     longitudinalPlan.visionCurrentLatAcc = 0.0
     longitudinalPlan.visionMaxPredLatAcc = 0.0
 
     pm.send('longitudinalPlan', plan_send)
+
+  def cruise_solutions(self, enabled, v_ego, a_ego, v_cruise, sm):
+    # Update controllers
+    self.vision_turn_controller.update(enabled, v_ego, a_ego, v_cruise, sm)
+
+    # Pick solution with lowest velocity target.
+    a_solutions = {'cruise': float("inf")}
+    v_solutions = {'cruise': v_cruise}
+
+    if self.vision_turn_controller.is_active:
+      a_solutions['turn'] = self.vision_turn_controller.a_target
+      v_solutions['turn'] = self.vision_turn_controller.v_turn
+
+    source = min(v_solutions, key=v_solutions.get)
+
+    return source, a_solutions[source], v_solutions[source]
