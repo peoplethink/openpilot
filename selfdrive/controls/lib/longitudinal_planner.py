a809345b007a7ee3224e79b9f0a63b9055815aff
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-03-27 11:12:05 +0900

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -113,7 +113,6 @@ class LongitudinalPlanner:
         sm['radarState'].leadOne,
         sm['modelV2'],
         sm['controlsState'],
-        sm['navInstruction'].maneuverDistance)
       self.mpc.mode = self.dynamic_experimental_controller.get_mpc_mode()
     else:
       self.mpc.mode = 'blended' if sm['controlsState'].experimentalMode else 'acc'
