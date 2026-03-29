3726f5990d89ee758bc71a44405bad3a997066ed
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-03-29 19:05:45 +0900

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env python3
 import os
 import numpy as np
- 
+
 from common.realtime import sec_since_boot
 from common.numpy_fast import clip, interp
 from selfdrive.swaglog import cloudlog
 from selfdrive.modeld.constants import index_function
 from selfdrive.controls.lib.radar_helpers import _LEAD_ACCEL_TAU
 from common.conversions import Conversions as CV
- 
+
 if __name__ == '__main__':  # generating code
   from third_party.acados.acados_template import AcadosModel, AcadosOcp, AcadosOcpSolver
 else:
   from selfdrive.controls.lib.longitudinal_mpc_lib.c_generated_code.acados_ocp_solver_pyx import AcadosOcpSolverCython  # pylint: disable=no-name-in-module, import-error
- 
+
 from casadi import SX, vertcat
- 
+
 MODEL_NAME = 'long'
 LONG_MPC_DIR = os.path.dirname(os.path.abspath(__file__))
 EXPORT_DIR = os.path.join(LONG_MPC_DIR, "c_generated_code")
 JSON_FILE = os.path.join(LONG_MPC_DIR, "acados_ocp_long.json")
- 
+
 SOURCES = ['lead0', 'lead1', 'cruise', 'e2e']
- 
+
 X_DIM = 3
 U_DIM = 1
 PARAM_DIM = 7
 COST_E_DIM = 5
 COST_DIM = COST_E_DIM + 1
 CONSTR_DIM = 4
- 
+
 X_EGO_OBSTACLE_COST = 3.
 X_EGO_COST = 0.
 V_EGO_COST = 0.
@@ -41,23 +41,32 @@ CRASH_DISTANCE = .25
 LEAD_DANGER_FACTOR = 0.8
 LIMIT_COST = 1e6
 ACADOS_SOLVER_TYPE = 'SQP_RTI'
- 
- 
+
+
 CRUISE_GAP_BP = [1., 2., 3., 4.]
 CRUISE_GAP_V = [1.0, 1.4, 2.0, 2.0]
 CRUISE_GAP_E2E_V = [1.3, 1.45, 1.6, 1.8]
- 
+
 AUTO_TR_BP = [0., 30.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 110.*CV.KPH_TO_MS]
 AUTO_TR_V = [1.0, 1.1, 1.2, 1.5]
- 
+
 AUTO_TR_CRUISE_GAP = 4
 DIFF_RADAR_VISION = 1.0
- 
- 
+
+# [FIX] 고속 컷인 급브레이크 방지 상수
+# x_obstacle[0]이 한 프레임에 이 값(m) 이상 줄면 컷인으로 판단해 ramp 발동
+# (새 lead 감지뿐 아니라 기존 추종 중 lead 교체도 감지)
+OBSTACLE_SUDDEN_DROP_THRESHOLD = 8.0
+# 속도별 ramp 감소폭 (m/frame, 20Hz 기준)
+# 고속일수록 빠르게 수렴해야 정상 추종도 자연스러움
+OBSTACLE_RAMP_BP_MS = [0., 30.*CV.KPH_TO_MS, 80.*CV.KPH_TO_MS, 120.*CV.KPH_TO_MS]
+OBSTACLE_RAMP_MAX_V = [2.0, 2.5, 3.5, 4.5]
+
+
 N = 12
 MAX_T = 10.0
 T_IDXS_LST = [index_function(idx, max_val=MAX_T, max_idx=N) for idx in range(N+1)]
- 
+
 T_IDXS = np.array(T_IDXS_LST)
 FCW_IDXS = T_IDXS < 5.0
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
@@ -67,12 +76,12 @@ T_FOLLOW = 1.45
 COMFORT_BRAKE = 2.5
 STOP_DISTANCE = 6.5
 STOP_DISTANCE_E2E = 6.0
- 
- 
+
+
 def get_stopped_equivalence_factor(v_lead, v_ego=0., t_follow=T_FOLLOW, stop_dist=STOP_DISTANCE, krkeegan=False):
   if not krkeegan:
     return (v_lead**2) / (2 * COMFORT_BRAKE)
- 
+
   # KRKeegan: lead 거리값을 고의로 늘려 solver가 더 빠른 가속을 유발하도록 함
   v_diff_offset = 0
   if np.all(v_lead - v_ego > 0):
@@ -81,35 +90,35 @@ def get_stopped_equivalence_factor(v_lead, v_ego=0., t_follow=T_FOLLOW, stop_dis
     v_diff_offset = np.maximum(v_diff_offset * ((10 - v_ego) / 10), 0)
   distance = (v_lead**2) / (2 * COMFORT_BRAKE) + v_diff_offset
   return distance
- 
- 
+
+
 def get_safe_obstacle_distance(v_ego, t_follow=T_FOLLOW, stop_dist=STOP_DISTANCE):
   return (v_ego**2) / (2 * COMFORT_BRAKE) + t_follow * v_ego + stop_dist
 
 def desired_follow_distance(v_ego, v_lead):
   return get_safe_obstacle_distance(v_ego) - get_stopped_equivalence_factor(v_lead)
- 
- 
+
+
 def gen_long_model():
   model = AcadosModel()
   model.name = MODEL_NAME
- 
+
   # set up states & controls
   x_ego = SX.sym('x_ego')
   v_ego = SX.sym('v_ego')
   a_ego = SX.sym('a_ego')
   model.x = vertcat(x_ego, v_ego, a_ego)
- 
+
   # controls
   j_ego = SX.sym('j_ego')
   model.u = vertcat(j_ego)
- 
+
   # xdot
   x_ego_dot = SX.sym('x_ego_dot')
   v_ego_dot = SX.sym('v_ego_dot')
   a_ego_dot = SX.sym('a_ego_dot')
   model.xdot = vertcat(x_ego_dot, v_ego_dot, a_ego_dot)
- 
+
   # live parameters
   a_min = SX.sym('a_min')
   a_max = SX.sym('a_max')
@@ -118,50 +127,50 @@ def gen_long_model():
   lead_t_follow = SX.sym('lead_t_follow')
   lead_danger_factor = SX.sym('lead_danger_factor')
   stop_dist = SX.sym('stop_dist')
- 
+
   model.p = vertcat(a_min, a_max, x_obstacle, prev_a, lead_t_follow, lead_danger_factor, stop_dist)
- 
+
   # dynamics model
   f_expl = vertcat(v_ego, a_ego, j_ego)
   model.f_impl_expr = model.xdot - f_expl
   model.f_expl_expr = f_expl
   return model
- 
- 
+
+
 def gen_long_ocp():
   ocp = AcadosOcp()
   ocp.model = gen_long_model()
- 
+
   Tf = T_IDXS[-1]
- 
+
   # set dimensions
   ocp.dims.N = N
- 
+
   # set cost module
   ocp.cost.cost_type = 'NONLINEAR_LS'
   ocp.cost.cost_type_e = 'NONLINEAR_LS'
- 
+
   QR = np.zeros((COST_DIM, COST_DIM))
   Q = np.zeros((COST_E_DIM, COST_E_DIM))
- 
+
   ocp.cost.W = QR
   ocp.cost.W_e = Q
- 
+
   x_ego, v_ego, a_ego = ocp.model.x[0], ocp.model.x[1], ocp.model.x[2]
   j_ego = ocp.model.u[0]
- 
+
   a_min, a_max = ocp.model.p[0], ocp.model.p[1]
   x_obstacle = ocp.model.p[2]
   prev_a = ocp.model.p[3]
   lead_t_follow = ocp.model.p[4]
   lead_danger_factor = ocp.model.p[5]
   stop_dist = ocp.model.p[6]
- 
+
   ocp.cost.yref = np.zeros((COST_DIM, ))
   ocp.cost.yref_e = np.zeros((COST_E_DIM, ))
- 
+
   desired_dist_comfort = get_safe_obstacle_distance(v_ego, lead_t_follow, stop_dist)
- 
+
   costs = [((x_obstacle - x_ego) - (desired_dist_comfort)) / (v_ego + 10.),
            x_ego,
            v_ego,
@@ -170,44 +179,44 @@ def gen_long_ocp():
            j_ego]
   ocp.model.cost_y_expr = vertcat(*costs)
   ocp.model.cost_y_expr_e = vertcat(*costs[:-1])
- 
+
   constraints = vertcat(v_ego,
                         (a_ego - a_min),
                         (a_max - a_ego),
                         ((x_obstacle - x_ego) - lead_danger_factor * (desired_dist_comfort)) / (v_ego + 10.))
   ocp.model.con_h_expr = constraints
- 
+
   x0 = np.zeros(X_DIM)
   ocp.constraints.x0 = x0
   ocp.parameter_values = np.array([-1.2, 1.2, 0.0, 0.0, T_FOLLOW, LEAD_DANGER_FACTOR, STOP_DISTANCE])
- 
+
   cost_weights = np.zeros(CONSTR_DIM)
   ocp.cost.zl = cost_weights
   ocp.cost.Zl = cost_weights
   ocp.cost.Zu = cost_weights
   ocp.cost.zu = cost_weights
- 
+
   ocp.constraints.lh = np.zeros(CONSTR_DIM)
   ocp.constraints.uh = 1e4*np.ones(CONSTR_DIM)
   ocp.constraints.idxsh = np.arange(CONSTR_DIM)
- 
+
   ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM'
   ocp.solver_options.hessian_approx = 'GAUSS_NEWTON'
   ocp.solver_options.integrator_type = 'ERK'
   ocp.solver_options.nlp_solver_type = ACADOS_SOLVER_TYPE
   ocp.solver_options.qp_solver_cond_N = 1
- 
+
   ocp.solver_options.qp_solver_iter_max = 10
   ocp.solver_options.qp_tol = 1e-3
- 
+
   # set prediction horizon
   ocp.solver_options.tf = Tf
   ocp.solver_options.shooting_nodes = T_IDXS
- 
+
   ocp.code_export_directory = EXPORT_DIR
   return ocp
- 
- 
+
+
 class LongitudinalMpc:
   def __init__(self, mode='acc'):
     self.mode = mode
@@ -222,10 +231,10 @@ class LongitudinalMpc:
     self._hf_a_change_multiplier = 1.0
     self._hf_danger_zone_multiplier = 1.0
     self._hf_danger_factor = LEAD_DANGER_FACTOR
-   
+
     self.reset()
     self.source = SOURCES[2]
- 
+
   def reset(self):
     self.solver.reset()
     self.v_solution = np.zeros(N+1)
@@ -253,6 +262,13 @@ class LongitudinalMpc:
     self.time_linearization = 0.0
     self.time_integrator = 0.0
     self.x0 = np.zeros(X_DIM)
+
+    # [FIX] 컷인 급브레이크 방지용 상태 변수
+    self.prev_x_obstacle   = None   # 이전 프레임 x_obstacle 배열 (N+1,)
+    self.prev_x_obstacle_0 = None   # 이전 프레임 x_obstacle[0] 스칼라
+    self.prev_lead_status  = False  # 이전 프레임 leadOne.status
+    self.cutin_ramp_active = False  # ramp 발동 중 여부
+
     self.set_weights()
 
   # ── Human-Like Following: 핵심 오프셋 계산 ──────────────────────────────
@@ -315,11 +331,11 @@ class LongitudinalMpc:
       W[4,4] = cost_weights[4] * np.interp(T_IDXS[i], [0.0, 1.0, 2.0], [1.0, 1.0, 0.0])
       self.solver.cost_set(i, 'W', W)
     self.solver.cost_set(N, 'W', np.copy(W[:COST_E_DIM, :COST_E_DIM]))
- 
+
     Zl = np.array(constraint_cost_weights)
     for i in range(N):
       self.solver.cost_set(i, 'Zl', Zl)
- 
+
   def get_cost_multipliers(self, v_lead0, v_lead1):
     v_ego = self.x0[1]
     v_ego_bps = [0, 10]
@@ -328,27 +344,27 @@ class LongitudinalMpc:
     a_change_tf = interp(self.t_follow, TFs, [.8, 1., 1.1])
     j_ego_tf    = interp(self.t_follow, TFs, [.8, 1., 1.1])
     d_zone_tf   = interp(self.t_follow, TFs, [1.3, 1., 1.])
- 
+
     j_ego_v_ego    = 1
     a_change_v_ego = 1
     if (v_lead0 - v_ego >= 0) and (v_lead1 - v_ego >= 0):
       j_ego_v_ego    = interp(v_ego, v_ego_bps, [.05, 1.])
       a_change_v_ego = interp(v_ego, v_ego_bps, [.05, 1.])
- 
+
     j_ego    = min(j_ego_tf, j_ego_v_ego)
     a_change = min(a_change_tf, a_change_v_ego)
     return (a_change, j_ego, d_zone_tf)
- 
+
   def set_weights(self, v_ego=0., a_desired=0., prev_accel_constraint=True, v_lead0=0, v_lead1=0):
     self.prev_accel_constraint = prev_accel_constraint
     self.a_desired = a_desired
- 
+
     if not prev_accel_constraint:
       self.prev_a = np.full(N+1, a_desired)
- 
+
     if self.mode == 'acc':
       a_change_cost = A_CHANGE_COST if prev_accel_constraint else 0
- 
+
       if self.applyLongDynamicCost:
         cost_multipliers = self.get_cost_multipliers(v_lead0, v_lead1)
 
@@ -372,14 +388,14 @@ class LongitudinalMpc:
           x_cost, v_cost, a_cost = 0., 0., 0.
         cost_weights = [X_EGO_OBSTACLE_COST, x_cost, v_cost, a_cost, a_change_cost, J_EGO_COST]
         constraint_cost_weights = [LIMIT_COST, LIMIT_COST, LIMIT_COST, DANGER_ZONE_COST]
- 
+
     elif self.mode == 'blended':
       cost_weights = [0., 0.1, 0.2, 5.0, 0.0, 1.0]
       constraint_cost_weights = [LIMIT_COST, LIMIT_COST, LIMIT_COST, 50.0]
     else:
       raise NotImplementedError(f'Planner mode {self.mode} not recognized in planner cost set')
     self.set_cost_weights(cost_weights, constraint_cost_weights)
- 
+
   def set_cur_state(self, v, a):
     v_prev = self.x0[1]
     self.x0[1] = v
@@ -387,7 +403,7 @@ class LongitudinalMpc:
     if abs(v_prev - v) > 2.:
       for i in range(0, N+1):
         self.solver.set(i, 'x', self.x0)
- 
+
   @staticmethod
   def extrapolate_lead(x_lead, v_lead, a_lead, a_lead_tau):
     a_lead_traj = a_lead * np.exp(-a_lead_tau * (T_IDXS**2)/2.)
@@ -395,7 +411,7 @@ class LongitudinalMpc:
     x_lead_traj = x_lead + np.cumsum(T_DIFFS * v_lead_traj)
     lead_xv = np.column_stack((x_lead_traj, v_lead_traj))
     return lead_xv
- 
+
   def process_lead(self, lead):
     v_ego = self.x0[1]
     if lead is not None and lead.status:
@@ -408,32 +424,32 @@ class LongitudinalMpc:
       v_lead = v_ego + 10.0
       a_lead = 0.0
       a_lead_tau = _LEAD_ACCEL_TAU
- 
+
     min_x_lead = ((v_ego + v_lead)/2) * (v_ego - v_lead) / (-MIN_ACCEL * 2)
     x_lead = clip(x_lead, min_x_lead, 1e8)
     v_lead = clip(v_lead, 0.0, 1e8)
     a_lead = clip(a_lead, -10., 5.)
     lead_xv = self.extrapolate_lead(x_lead, v_lead, a_lead, a_lead_tau)
     return lead_xv
- 
+
   def set_accel_limits(self, min_a, max_a):
     self.cruise_min_a = min_a
     self.max_a = max_a
- 
+
   def update(self, carstate, radarstate, v_cruise, x, v, a, j):
     v_ego = self.x0[1]
     self.status = radarstate.leadOne.status or radarstate.leadTwo.status
- 
+
     lead_xv_0 = self.process_lead(radarstate.leadOne)
     lead_xv_1 = self.process_lead(radarstate.leadTwo)
- 
+
     # neokii
     cruise_gap = int(clip(carstate.cruiseGap, 1., 4.)) if carstate.cruiseGap > 0 else AUTO_TR_CRUISE_GAP
     if cruise_gap == AUTO_TR_CRUISE_GAP:
       tr = interp(carstate.vEgo, AUTO_TR_BP, AUTO_TR_V) if self.mode == 'acc' else T_FOLLOW
     else:
       tr = interp(float(cruise_gap), CRUISE_GAP_BP, CRUISE_GAP_V if self.mode == 'acc' else CRUISE_GAP_E2E_V)
- 
+
     self.t_follow = tr
     self.stop_dist = STOP_DISTANCE if self.mode == 'acc' else STOP_DISTANCE_E2E
 
@@ -459,17 +475,17 @@ class LongitudinalMpc:
                      prev_accel_constraint=self.prev_accel_constraint,
                      v_lead0=lead_xv_0[0, 1],
                      v_lead1=lead_xv_1[0, 1])
- 
+
     lead_0_obstacle = lead_xv_0[:,0] + get_stopped_equivalence_factor(
       lead_xv_0[:,1], self.x_sol[:,1], self.t_follow, self.stop_dist,
       krkeegan=self.applyLongDynamicCost)
     lead_1_obstacle = lead_xv_1[:,0] + get_stopped_equivalence_factor(
       lead_xv_1[:,1], self.x_sol[:,1], self.t_follow, self.stop_dist,
       krkeegan=self.applyLongDynamicCost)
- 
+
     self.params[:,0] = MIN_ACCEL
     self.params[:,1] = self.max_a
- 
+
     if self.mode == 'acc':
       # ── Human-Like Following: danger_factor를 params에 반영 ──
       self.params[:,5] = self._hf_danger_factor if self.human_following else LEAD_DANGER_FACTOR
@@ -483,32 +499,73 @@ class LongitudinalMpc:
       cruise_obstacle = np.cumsum(T_DIFFS * v_cruise_clipped) + get_safe_obstacle_distance(v_cruise_clipped, self.t_follow, self.stop_dist)
       x_obstacles = np.column_stack([lead_0_obstacle, lead_1_obstacle, cruise_obstacle])
       self.source = SOURCES[np.argmin(x_obstacles[0])]
- 
+
       cruise_target = T_IDXS * np.clip(v_cruise, v_ego - 2.0, 1e3) + x[0]
       xforward = ((v[1:] + v[:-1]) / 2) * (T_IDXS[1:] - T_IDXS[:-1])
       x = np.cumsum(np.insert(xforward, 0, x[0]))
- 
+
       x_and_cruise = np.column_stack([x, cruise_target])
       x = np.min(x_and_cruise, axis=1)
- 
+
     elif self.mode == 'blended':
- 
+
       self.params[:,5] = 1.0
- 
+
       x_obstacles = np.column_stack([lead_0_obstacle,
                                      lead_1_obstacle])
       cruise_target = T_IDXS * np.clip(v_cruise, v_ego - 2.0, 1e3) + x[0]
       xforward = ((v[1:] + v[:-1]) / 2) * (T_IDXS[1:] - T_IDXS[:-1])
       x = np.cumsum(np.insert(xforward, 0, x[0]))
- 
+
       x_and_cruise = np.column_stack([x, cruise_target])
       x = np.min(x_and_cruise, axis=1)
- 
+
       self.source = 'e2e' if x_and_cruise[1,0] < x_and_cruise[1,1] else 'cruise'
- 
+
     else:
       raise NotImplementedError(f'Planner mode {self.mode} not recognized in planner update')
- 
+
+    # ── [FIX] 고속 컷인 급브레이크 방지: x_obstacle ramp 적용 ───────────────
+    # 기존 문제:
+    #   prev_lead_status(False→True) 조건만 체크 →
+    #   이미 앞차 추종 중(True→True)에 컷인차가 lead 자리를 빼앗는
+    #   고속도로 컷인은 ramp 미발동 → x_obstacle 한 프레임 수십m 점프
+    #   → MPC 즉시 최대 제동 → 움찔
+    #
+    # 수정:
+    #   1. 새 lead 감지(False→True): ramp 발동
+    #   2. 기존 추종 중에도 x_obstacle[0]이 OBSTACLE_SUDDEN_DROP_THRESHOLD 이상
+    #      한 프레임 감소 시 컷인 판단, ramp 발동
+    #   3. 속도 비례 ramp 감소폭 (고속일수록 빠르게 수렴)
+    #   4. 수렴 완료(오차 < 0.5m)까지 cutin_ramp_active 유지
+    #   5. FCW(crash_cnt > 0): ramp 즉시 해제 → 긴급 제동 보장
+    min_x_obstacles = np.min(x_obstacles, axis=1)
+
+    ramp_max = float(interp(v_ego, OBSTACLE_RAMP_BP_MS, OBSTACLE_RAMP_MAX_V))
+
+    if self.prev_x_obstacle is not None and self.crash_cnt == 0:
+      new_lead    = radarstate.leadOne.status and not self.prev_lead_status
+      sudden_drop = (self.prev_x_obstacle_0 is not None and
+                     (self.prev_x_obstacle_0 - min_x_obstacles[0]) > OBSTACLE_SUDDEN_DROP_THRESHOLD)
+
+      if new_lead or sudden_drop or self.cutin_ramp_active:
+        ramped = np.maximum(min_x_obstacles,
+                            self.prev_x_obstacle - ramp_max)
+        min_x_obstacles = ramped
+        # 목표값 수렴 완료 시 ramp 해제
+        self.cutin_ramp_active = bool(
+          np.any(ramped - np.min(x_obstacles, axis=1) > 0.5)
+        )
+      else:
+        self.cutin_ramp_active = False
+    else:
+      self.cutin_ramp_active = False
+
+    self.prev_x_obstacle   = min_x_obstacles.copy()
+    self.prev_x_obstacle_0 = float(min_x_obstacles[0])
+    self.prev_lead_status  = radarstate.leadOne.status
+    # ─────────────────────────────────────────────────────────────────────
+
     self.yref[:,1] = x
     self.yref[:,2] = v
     self.yref[:,3] = a
@@ -516,58 +573,59 @@ class LongitudinalMpc:
     for i in range(N):
       self.solver.set(i, "yref", self.yref[i])
     self.solver.set(N, "yref", self.yref[N][:COST_E_DIM])
- 
-    self.params[:,2] = np.min(x_obstacles, axis=1)
+
+    # [FIX] ramp 적용된 min_x_obstacles 사용
+    self.params[:,2] = min_x_obstacles
     self.params[:,3] = np.copy(self.prev_a)
     self.params[:,4] = self.t_follow
     self.params[:,6] = self.stop_dist
- 
+
     self.run()
     if (np.any(lead_xv_0[FCW_IDXS,0] - self.x_sol[FCW_IDXS,0] < CRASH_DISTANCE) and
             radarstate.leadOne.modelProb > 0.9):
       self.crash_cnt += 1
     else:
       self.crash_cnt = 0
- 
+
     if self.mode == 'blended':
       if any((lead_0_obstacle - get_safe_obstacle_distance(self.x_sol[:,1], self.t_follow, self.stop_dist)) - self.x_sol[:, 0] < 0.0):
         self.source = 'lead0'
       if any((lead_1_obstacle - get_safe_obstacle_distance(self.x_sol[:,1], self.t_follow, self.stop_dist)) - self.x_sol[:, 0] < 0.0) and \
          (lead_1_obstacle[0] - lead_0_obstacle[0]):
         self.source = 'lead1'
- 
- 
+
+
   def run(self):
     for i in range(N+1):
       self.solver.set(i, 'p', self.params[i])
     self.solver.constraints_set(0, "lbx", self.x0)
     self.solver.constraints_set(0, "ubx", self.x0)
- 
+
     self.solution_status = self.solver.solve()
     self.solve_time = float(self.solver.get_stats('time_tot')[0])
     self.time_qp_solution = float(self.solver.get_stats('time_qp')[0])
     self.time_linearization = float(self.solver.get_stats('time_lin')[0])
     self.time_integrator = float(self.solver.get_stats('time_sim')[0])
- 
+
     for i in range(N+1):
       self.x_sol[i] = self.solver.get(i, 'x')
     for i in range(N):
       self.u_sol[i] = self.solver.get(i, 'u')
- 
+
     self.v_solution = self.x_sol[:,1]
     self.a_solution = self.x_sol[:,2]
     self.j_solution = self.u_sol[:,0]
- 
+
     self.prev_a = np.interp(T_IDXS + 0.05, T_IDXS, self.a_solution)
- 
+
     t = sec_since_boot()
     if self.solution_status != 0:
       if t > self.last_cloudlog_t + 5.0:
         self.last_cloudlog_t = t
         cloudlog.warning(f"Long mpc reset, solution_status: {self.solution_status}")
       self.reset()
- 
- 
+
+
 if __name__ == "__main__":
   ocp = gen_long_ocp()
   AcadosOcpSolver.generate(ocp, json_file=JSON_FILE)
